82aa2b26cdb39f0f366bb7b4d530a99602cb4a2d
aman2801 <amandeep.2801singh@gmail.com>
2025-08-08 00:47:32 +0530

    debugging

--- api/server.js
+++ api/server.js
@@ -40,10 +40,4 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 module.exports = app;
